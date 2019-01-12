bd522a09763312421815134e93599fb24b026238
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-01-12 21:39:43 +0530

    shell checks

--- buildah.sh
+++ buildah.sh
@@ -3,27 +3,27 @@
 # define the container base image
 javacontainer=$(buildah from registry.fedoraproject.org/fedora-minimal)
 # mount the container root FS
-javamnt=$(buildah mount $javacontainer)
+javamnt=$(buildah mount "$javacontainer")
 
 # make the java app directory
-mkdir -pv $javamnt/work
+mkdir -pv "$javamnt/work"
 
 # copy the application jar, with Knative build templates, the app sources gets loaded in the /workspace directory
 # adjust application name accordingly
-if [ -z "$CONTEXT_DIR" || "$CONTEXT_DIR" ="."] 
+if [ -z "$CONTEXT_DIR"  ] || [ "$CONTEXT_DIR" = "." ] 
 then
-  cp -v /workspace/target/${JAVA_APP_NAME} $javamnt/work
+  cp -v "/workspace/target/${JAVA_APP_NAME}" "$javamnt/work"
 else
-  cp -v /workspace/${CONTEXT_DIR}/target/${JAVA_APP_NAME} $javamnt/work
+  cp -v "/workspace/${CONTEXT_DIR}/target/${JAVA_APP_NAME}" "$javamnt/work"
 fi
 
-chmod -R 755 $javamnt/work
+chmod -R 755 "$javamnt/work"
 
-buildah config --workingdir /work $javacontainer
+buildah config --workingdir /work "$javacontainer"
 buildah config --port 8080
-buildah config --entrypoint '["./app","-Dshamrock.http.host=0.0.0.0"]' $javacontainer
+buildah config --entrypoint '["./app","-Dshamrock.http.host=0.0.0.0"]' "$javacontainer"
 
-imageID=$(buildah commit $javacontainer $IMAGE_NAME)
+imageID=$(buildah commit "$javacontainer" "$IMAGE_NAME")
 
 # Push the image back to local default docker registry
 # you can also push to external registry 
@@ -37,6 +37,6 @@ imageID=$(buildah commit $javacontainer $IMAGE_NAME)
 
 ## HTTP
 buildah push --tls-verify=false \
-  --creds=openshift:$(cat /var/run/secrets/kubernetes.io/serviceaccount/token) \
-   $imageID \
-   docker://docker-registry.default.svc:5000/$POD_NAMESPACE/$IMAGE_NAME
+  --creds=openshift:"$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+   "$imageID" \
+   "docker://docker-registry.default.svc:5000/$POD_NAMESPACE/$IMAGE_NAME"
